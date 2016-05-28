95acf8eaf3288ad1e9d94798d9cc83c21ae548c6
howchmo <howardmall+bitbucket@howardmall.net>
2016-05-28 15:08:47 -0400

    Convert typed array to a normal array before you send it via smartfox API

--- js/uspeak.js
+++ js/uspeak.js
@@ -1,9 +1,44 @@
 var sfs;
 var config = {};
 var roomid = "MainLobby#_@LocalVoip#_@0655795e-9916-4cfc-be1e-81f544d1d7cb#_@public";
-var file = new Int8Array(64000);
 var startTime = 0;
+var sampleSize = 256;
+var userid="1";
+
 var audioCtx = new AudioContext();
+
+navigator.getUserMedia = (
+	navigator.getUserMedia ||
+	navigator.webkitGetUserMedia ||
+	navigator.mozGetUserMedia ||
+	navigator.msGetUserMedia
+);
+
+// adapted from http://stackoverflow.com/questions/35234551/javascript-converting-from-int16-to-float32
+function float32To16Bit(inputArray)
+{
+	var output = new Int16Array(inputArray.length);
+	for (var i = 0; i < inputArray.length; i++)
+	{
+		var s = Math.max(-1, Math.min(1, inputArray[i]));
+		output[i] = s < 0 ? s * 0x8000 : s * 0x7FFF;
+	}
+	return output;
+}
+
+// adapted from http://stackoverflow.com/questions/35234551/javascript-converting-from-int16-to-float32
+function int16ToFloat32(inputArray)
+{
+	var output = new Float32Array(inputArray.length);
+	for (var i = 0; i < inputArray.length; i++)
+	{
+		var int = inputArray[i];
+		// If the high bit is on, then it is a negative number, and actually counts backwards.
+		var float = (int >= 0x8000) ? -(0x10000 - int) / 0x8000 : int / 0x7FFF;
+		output[i] = float;
+	}
+	return output;
+}
 
 // This is a port directly from the USpeak C# source code
 function MuLawDecode( mulaw )
@@ -22,6 +57,26 @@ function MuLawDecode( mulaw )
 		return data;
 	else
 		return -data;
+}
+
+function MuLawEncode( /* int32 */ pcm )
+{
+	var BIAS = 0x84;
+	var MAX = 32635;
+
+	var sign = ( pcm & 0x8000 ) >> 8;
+	if( sign != 0 )
+		pcm = -pcm;
+	if( pcm > MAX )
+		pcm = MAX;
+	pcm += BIAS;
+	var exponent = 7;
+	for( expMask = 0x4000; ( pcm & expMask ) == 0; exponent--, expMask >>= 1 )
+	{
+	}
+	var mantissa = ( pcm >> ( exponent + 3 ) ) & 0x0f;
+	var mulaw = ( sign | exponent << 4 | mantissa );
+	return ~mulaw;
 }
 
 function playPCMclip( buffer )
@@ -124,9 +179,12 @@ function onExtensionResponse( evt )
 	if( requestType == "VoipVCRequest" )
 	{
 		var params = evt.params;
-		var userid = params["ui"];
-		var data = params["VCData"];
-		playMuLawClip(data);
+		if( true ) // params["ui"] != userid )
+		{
+			var data = params["VCData"];
+			console.log(data);
+			playMuLawClip(data);
+		}
 	}
 }
 
@@ -158,6 +216,68 @@ function connect( host, port, zone, voipRoom )
 	sfs.connect(config.host, config.port );
 }
 
+function sendAudio( floats )
+{
+	var data = new Uint8Array(floats.length+6);
+	data[0] = sampleSize-1;
+	data[1] = 0;
+	data[2] = 0;
+	data[3] = 0;
+	data[4] = 0;
+	data[5] = 0;
+	for( i=0; i<floats.length; i++ )
+	{
+		data[i+6] = MuLawEncode(Math.round(floats[i]*3267));
+	}
+	var params = {};
+	params["ui"] = userid;
+	params["VCData"] = Array.prototype.slice.call(data);
+	sfs.send(new SFS2X.Requests.System.ExtensionRequest("VoipVCRequest", params));
+}
+
+function setupAudioNode( stream )
+{
+	sourceNode = audioContext.createMediaStreamSource(stream);
+	javascriptNode = audioContext.createScriptProcessor(sampleSize, 1, 1);
+	javascriptNode.onaudioprocess = function(e)
+	{
+		sendAudio( new Float32Array(e.inputBuffer.getChannelData(0)));
+	}
+	sourceNode.connect(javascriptNode);
+	javascriptNode.connect(audioContext.destination);
+}
+
+function onAudioError(e)
+{
+	console.log(e);
+	alert(e);
+}
+
+function initializeMic()
+{
+	try
+	{
+		audioContext = new AudioContext();
+	}
+	catch(e)
+	{
+		alert('Web Audio API is not supported in this browser');
+	}
+
+	try
+	{
+		navigator.getUserMedia(
+			{ video: false, audio: true},
+			setupAudioNode,
+			onAudioError
+		);
+	}
+	catch (e)
+	{
+		alert('webkitGetUserMedia threw exception :' + e);
+	}
+}
+
 function init()
 {
 	var host = document.getElementById("host").value;
@@ -166,6 +286,8 @@ function init()
 	var room = document.getElementById("room").value;
 	console.log(host, port, zone, room);
 	connect(host, port, zone, room);
+
+	initializeMic();
 }
 
 
